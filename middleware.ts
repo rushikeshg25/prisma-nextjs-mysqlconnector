e4db205e3dd222341a847bc99c3407c8d328304f
rushikeshg25 <rushikeshvghotekar25@gmail.com>
2024-01-06 13:23:39 +0530

    Added protected routes and middleware

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,8 @@
 import authConfig from "./auth.config"
 import NextAuth from "next-auth"
  const { auth  } = NextAuth(authConfig)
-
+ import {authRoutes,apiAuthPrefix,DEFAULT_LOGIN_REDIRECT,publicRoutes} from "@/routes"
+ 
 export default auth((req) => {
   // req.auth
 //   console.log(req)
